ea82575736f451cf54ed899bf67358d3a832bd36
Lionel Maquet <lionel.maquet@gmail.com>
2019-12-03 15:08:03 +0100

    testing db imp exp

--- neutralcrm.sql
+++ neutralcrm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 03 déc. 2019 à 14:01
--- Version du serveur :  5.7.23
--- Version de PHP :  7.2.10
+-- Généré le :  mar. 03 déc. 2019 à 14:06
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `todo` (
   `progress` varchar(30) COLLATE latin1_general_ci NOT NULL,
   `estimated_time` int(5) NOT NULL,
   PRIMARY KEY (`ind`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Déchargement des données de la table `todo`
@@ -52,7 +52,8 @@ INSERT INTO `todo` (`ind`, `user`, `title`, `progress`, `estimated_time`) VALUES
 (7, 'lio', 'encours', 'fraipont', 10),
 (8, 'gul', 'avenir', 'fraipont', 10),
 (9, 'lio', 'neutral crm', 'termine', 10),
-(10, 'lio', 'neutral crm', 'encours', 10);
+(10, 'lio', 'neutral crm', 'encours', 10),
+(11, 'ju', 'tesettest', 'encours', 3);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
