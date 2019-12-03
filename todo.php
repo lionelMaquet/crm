b035c5aa9e73b4849d5e6cf28d7242c5cf4282f2
Lionel Maquet <lionel.maquet@gmail.com>
2019-12-03 16:43:44 +0100

    ...

--- todo.php
+++ todo.php
@@ -11,12 +11,13 @@ $progress : "encours" ou "avenir" ou "termine"
 function getTodoInfo($user,$progress) {
   global $mysqli;
   $stmt = ("SELECT title FROM todo WHERE user = '".$user."' AND progress = '".$progress."'");
-  
+
   $result = $mysqli->query($stmt);
   while ($row = $result->fetch_assoc()) {
         printf ($row['title']);
     }
 }
+
 
 
 if (isset($_GET['user']) && isset($_GET['etat']) && isset($_GET['title']) ) {
@@ -29,6 +30,7 @@ if (isset($_GET['user']) && isset($_GET['etat']) && isset($_GET['title']) ) {
   $stmt = ("INSERT INTO todo (user, title, progress, estimated_time) VALUES ('".$user."', '".$title."', '".$etat."', 10) ");
   $mysqli->query($stmt);
   header("Location: http://www.localhost/crm/index.php");
+  
 }
 
 
