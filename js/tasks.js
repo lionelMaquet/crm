19d5e293d623bf645cd93635f4ea76d501db2a88
Lionel <lionel@MacBook-Pro-de-Lionel.local>
2020-01-14 16:30:56 +0100

    le premier user par projet est sélectionné par défaut dans les détails d'un projet

--- js/tasks.js
+++ js/tasks.js
@@ -1,43 +1,50 @@
 // GET TASKS
 
 
-function replaceTasksWith(list){
+function refreshListOfTasks(username){
 
-  $('#listeTaches').html(list);
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open("GET", "gettasks.php?user="+username+"&projet_id="+projectID, true);
+
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          let listeTache = this.responseText;
+          $('#listeTaches').html(listeTache);
+
+      }
+  };
+
+  xmlhttp.send();
+
+}
+
+function selectUserForTask(target){ // target is the p
+
+  $('.username').removeClass('selectedForTasks');
+  $(target).addClass('selectedForTasks');
+  $('.hiddenValueCurrentUser').val(target.id) // pour l'ajout de taches, savoir à qui l'ajouter
+  $('#nameOfCurrentUserForTasks').html(target.id)
+  refreshListOfTasks(target.id)
 
 }
 
 $(document).on('click',".username", function(){
 
-  let user = event.target.id
-  let id = projectID
-  console.log(user)
-  console.log("hi")
-
-  console.log(event.target)
-
-  $('.username').removeClass('selectedForTasks');
-
-  $(event.target).addClass('selectedForTasks');
-
-  $('.hiddenValueCurrentUser').val(user) // pour l'ajout de taches, savoir à qui l'ajouter
-
-
-  $('#nameOfCurrentUserForTasks').html(user)
-
-  var xmlhttp = new XMLHttpRequest();
-
-  xmlhttp.open("GET", "gettasks.php?user="+user+"&projet_id="+id, true);
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          let listeTache = this.responseText;
-          replaceTasksWith(listeTache);
-
-      }
-  };
-  xmlhttp.send();
+  selectUserForTask(event.target)
 
 })
 
 
-// ADD TASK
+
+
+// le double $$ est pour convertir l'élément en objet jquery. Sinon le hasClass() ne fonctionne pas
+if (!$($('.username').get(0)).hasClass('hiddenColor')){
+  selectUserForTask($('.username')[0])
+}
+
+else if (!$($('.username').get(1)).hasClass('hiddenColor')){
+  selectUserForTask($('.username')[1])}
+
+else if (!$($('.username').get(2)).hasClass('hiddenColor')){
+    selectUserForTask($('.username')[2])}
