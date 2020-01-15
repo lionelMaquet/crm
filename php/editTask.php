59c59d9afca4690bf0d13e4fb9e4c3113a8562c0
Lionel <lionel.maquet@gmail.com>
2020-01-15 13:35:24 +0100

    optimisation php

--- php/editTask.php
+++ php/editTask.php
@@ -1,32 +1,37 @@
 <?php
 
 require 'connection.php';
-$taskID = $_GET['taskid'];
 
-$sql = "SELECT `fait` FROM tache WHERE tache_id = " . $taskID;
-$result = $conn->query($sql);
+if ($_GET['q'] == 'taskstatut') {
+
+  $taskID = $_GET['taskid'];
+
+  $sql = "SELECT `fait` FROM tache WHERE tache_id = " . $taskID;
+  $result = $conn->query($sql);
+
+  $statutActuel;
+
+  if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+          $statutActuel = $row['fait'];
+      }
+  }
 
 
+  if ($statutActuel == 'oui') {
+    $sql = "UPDATE tache SET `fait` = 'non' WHERE `tache_id` = ".$taskID;
+    $result = $conn->query($sql);
+    echo "fait updaté en 'non'";
+  } else if($statutActuel == 'non' ){
+    $sql = "UPDATE tache SET `fait` = 'oui' WHERE `tache_id` = ".$taskID;
+    $result = $conn->query($sql);
+    echo "fait updaté en 'oui'";
+  }
 
-$statutActuel;
-
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $statutActuel = $row['fait'];
-    }
 }
 
 
-if ($statutActuel == 'oui') {
-  $sql = "UPDATE tache SET `fait` = 'non' WHERE `tache_id` = ".$taskID;
-  $result = $conn->query($sql);
-  echo "fait updaté en 'non'";
-} else if($statutActuel == 'non' ){
-  $sql = "UPDATE tache SET `fait` = 'oui' WHERE `tache_id` = ".$taskID;
-  $result = $conn->query($sql);
-  echo "fait updaté en 'oui'";
-}
+
 
 
 
